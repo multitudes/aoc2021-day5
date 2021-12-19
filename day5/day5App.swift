b55bde680daea6b3efa09114990b387335a9331c
laurent brusa <wrmultitudes@gmail.com>
2021-12-19 15:38:09 +0100

    Initial Commit

---
+++ day5/day5App.swift
@@ -0,0 +1,17 @@
+//
+//  day5App.swift
+//  day5
+//
+//  Created by Laurent B on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct day5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
